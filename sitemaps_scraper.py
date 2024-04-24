d2d0b35eb420400976989841699cef947add40bc
flopot <48916418+flopot@users.noreply.github.com>
2024-04-24 21:41:25 +0200

    Add files via upload

---
+++ sitemaps_scraper.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+from advertools import sitemap_to_df
+
+# Streamlit app title
+st.title('Sitemap URL Scraper')
+
+# User input for URL
+sitemap_url = st.text_input('Enter the URL of the sitemap', 'https://www.WEBSITE.com/sitemap.xml')
+
+# Button to execute the scraping
+if st.button('Scrape Sitemap'):
+    if sitemap_url:
+        # Use sitemap_to_df to extract URLs
+        df = sitemap_to_df(sitemap_url)
+        
+        # Display the DataFrame in the app (optional)
+        st.write(df[['loc', 'sitemap']])
+        
+        # Convert DataFrame to CSV for download
+        csv = df[['loc', 'sitemap']].to_csv(index=False).encode('utf-8')
+        
+        # Create a button to download the CSV file
+        st.download_button(label="Download URLs as CSV",
+                           data=csv,
+                           file_name="sitemap_urls.csv",
+                           mime='text/csv')
+    else:
+        st.error("Please enter a valid URL.")

9fdae7ecc4016db95a02abcc60d4965b6c55f181
flopot <48916418+flopot@users.noreply.github.com>
2024-07-11 17:31:39 +0200

    Update sitemaps_scraper.py

--- sitemaps_scraper.py
+++ sitemaps_scraper.py
@@ -5,8 +5,20 @@ from advertools import sitemap_to_df
 # Custom CSS for styling and external stylesheet
 st.markdown(
     """
+    <link rel="stylesheet" id="astra-theme-css-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/themes/astra/assets/css/minified/main.min.css?ver=4.6.8" media="all" href="https://www.intrepidonline.com/wp-content/themes/astra/assets/css/minified/main.min.css?ver=4.6.8">
+    <link media="all" onload="this.media='all';this.onload=null;" rel="stylesheet" id="astra-google-fonts-css" href="https://www.intrepidonline.com/wp-content/cache/perfmatters/www.intrepidonline.com/fonts/8f516dc4344f.google-fonts.css">
+    <link rel="stylesheet" id="astra-theme-dynamic-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/uploads/astra/astra-theme-dynamic-css-post-7386.css?ver=1720711612" media="all" href="https://www.intrepidonline.com/wp-content/uploads/astra/astra-theme-dynamic-css-post-7386.css?ver=1720711612">
+    <link rel="stylesheet" id="additional-styles-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/themes/astra-child/dist/css/astraChild.min.css?ver=1.002" media="all" href="https://www.intrepidonline.com/wp-content/themes/astra-child/dist/css/astraChild.min.css?ver=1.002">
+    <link rel="stylesheet" id="astra-addon-css-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-668f8149383bd2-70380954.css?ver=4.6.8" media="all" href="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-668f8149383bd2-70380954.css?ver=4.6.8">
+    <link rel="stylesheet" id="astra-addon-dynamic-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-dynamic-css-post-7386.css?ver=1720711612" media="all" href="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-dynamic-css-post-7386.css?ver=1720711612">
+    <link rel="stylesheet" id="elementor-frontend-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.22.1" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.22.1">
+    <link rel="stylesheet" id="swiper-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min.css?ver=8.4.5" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min.css?ver=8.4.5">
+    <link rel="stylesheet" id="powerpack-frontend-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/powerpack-lite-for-elementor/assets/css/min/frontend.min.css?ver=2.7.19" media="all" href="https://www.intrepidonline.com/wp-content/plugins/powerpack-lite-for-elementor/assets/css/min/frontend.min.css?ver=2.7.19">
+    <link rel="stylesheet" id="elementor-pro-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.22.0" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.22.0">
+    <link rel="stylesheet" id="font-awesome-5-all-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/all.min.css?ver=3.22.1" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/all.min.css?ver=3.22.1">
+    <link rel="stylesheet" id="font-awesome-4-shim-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/v4-shims.min.css?ver=3.22.1" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/v4-shims.min.css?ver=3.22.1">
+    <link media="all" onload="this.media='all';this.onload=null;" rel="stylesheet" id="google-fonts-1-css" href="https://www.intrepidonline.com/wp-content/cache/perfmatters/www.intrepidonline.com/fonts/3fc4160e26bd.google-fonts.css">
     <style>
-    @import url('https://intrepiddigstg.wpengine.com/wp-content/cache/perfmatters/intrepiddigstg.wpengine.com/fonts/8f516dc4344f.google-fonts.css');
     </style>
     """,
     unsafe_allow_html=True

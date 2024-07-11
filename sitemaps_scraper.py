9b81454c056cdb33063659009faa27a46318c702
flopot <48916418+flopot@users.noreply.github.com>
2024-07-11 17:34:21 +0200

    Update sitemaps_scraper.py

--- sitemaps_scraper.py
+++ sitemaps_scraper.py
@@ -5,11 +5,6 @@ from advertools import sitemap_to_df
 # Custom CSS for styling and external stylesheet
 st.markdown(
     """
-    <link rel="stylesheet" id="astra-addon-css-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-668f8149383bd2-70380954.css?ver=4.6.8" media="all" href="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-668f8149383bd2-70380954.css?ver=4.6.8">
-    <link rel="stylesheet" id="astra-addon-dynamic-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-dynamic-css-post-7386.css?ver=1720711612" media="all" href="https://www.intrepidonline.com/wp-content/uploads/astra-addon/astra-addon-dynamic-css-post-7386.css?ver=1720711612">
-    <link rel="stylesheet" id="powerpack-frontend-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/powerpack-lite-for-elementor/assets/css/min/frontend.min.css?ver=2.7.19" media="all" href="https://www.intrepidonline.com/wp-content/plugins/powerpack-lite-for-elementor/assets/css/min/frontend.min.css?ver=2.7.19">
-    <link rel="stylesheet" id="elementor-pro-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.22.0" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.22.0">
-    <link rel="stylesheet" id="font-awesome-5-all-css" data-pmdelayedstyle="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/all.min.css?ver=3.22.1" media="all" href="https://www.intrepidonline.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/all.min.css?ver=3.22.1">
     <style>
     </style>
     """,

af5edd3c32089f99f0e86b84f126dd106d62e739
flopot <48916418+flopot@users.noreply.github.com>
2024-07-11 17:42:06 +0200

    Update sitemaps_scraper.py

--- sitemaps_scraper.py
+++ sitemaps_scraper.py
@@ -6,6 +6,14 @@ from advertools import sitemap_to_df
 st.markdown(
     """
     <style>
+    .stHeadingWithActionElements,.stMarkdown,.stMarkdownContainer,.st-ae,.st-bd,.st-be,.st-bf,.st-bg,.st-bh,.st-bi,.st-bj,.st-bk,.st-bl,.st-bm,.st-ah,.st-bn,.st-bo,.st-bp,.st-bq,.st-br,.st-bs,.st-bt,.st-bu,.st-ax,.st-ay,.st-az,.st-bv,.st-b1,.st-b2,.st-bc,.st-bw,.st-bx,.st-by{
+        color: black
+    }
+
+    appview-container, button {
+        background-color: white
+    }
+    
     </style>
     """,
     unsafe_allow_html=True

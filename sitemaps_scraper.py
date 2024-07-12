930056abcb8a3658b736ad95df8bc6685bfbe6e1
flopot <48916418+flopot@users.noreply.github.com>
2024-07-12 16:29:18 +0200

    Update sitemaps_scraper.py

--- sitemaps_scraper.py
+++ sitemaps_scraper.py
@@ -8,9 +8,13 @@ st.markdown(
     <style>
         p,.appview-container,h1,.stHeadingWithActionElements,.stWidgetLabel,.stMarkdown,.st-ae,.st-bd,.st-be,.st-bf,.st-bg,.st-bh,.st-bi,.st-bj,.st-bk,.st-bl,.st-bm,.st-ah,.st-bn,.st-bo,.st-bp,.st-bq,.st-br,.st-bs,.st-bt,.st-bu,.st-ax,.st-ay,.st-az,.st-bv,.st-b1,.st-b2,.st-bc,.st-bw,.st-bx,.st-by{
         color: black !important;
-        background-color: white !important;
         font-family: "Raleway", Sans-serif;
         }
+
+        .appview-container,h1,.stHeadingWithActionElements,.stWidgetLabel,.stMarkdown,.st-ae,.st-bd,.st-be,.st-bf,.st-bg,.st-bh,.st-bi,.st-bj,.st-bk,.st-bl,.st-bm,.st-ah,.st-bn,.st-bo,.st-bp,.st-bq,.st-br,.st-bs,.st-bt,.st-bu,.st-ax,.st-ay,.st-az,.st-bv,.st-b1,.st-b2,.st-bc,.st-bw,.st-bx,.st-by{
+        background-color: white !important;
+        }
+        
         button{
         background-color: #1098A7 !important;
         border: none;
